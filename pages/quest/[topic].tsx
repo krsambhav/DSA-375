e69399ce3cf4f3d7090164db8b98dd59389ce8d6
Kumar Sambhav <krsambhav16@gmail.com>
2022-05-28 23:01:50 +0530

    Added Data

--- pages/quest/[topic].tsx
+++ pages/quest/[topic].tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
 import QuestionCard from "../../components/QuestionCard";
 import questionData from '../../public/data.json';
+import {IoIosArrowRoundBack} from 'react-icons/io';
+import Link from "next/link";
 
 export default function Questions() {
   const [questions, setQuestions] = useState<any>();
@@ -26,8 +28,8 @@ export default function Questions() {
     <>
     <Head><title>{title}</title></Head>
     <main className="w-screen p-10 flex flex-col items-center">
-      <div className="title-container text-3xl">
-        {readyToShow && title}
+      <div className="title-container text-3xl flex flex-row items-center justify-between w-[95vw]">
+        <div className="icon"><Link href={'/'}><a><IoIosArrowRoundBack /></a></Link></div> <div className="title">{readyToShow && title}</div> <div><IoIosArrowRoundBack className="opacity-0" /></div>
       </div>
       <div className="questions-container flex flex-row flex-wrap items-center justify-center gap-10 mt-10">
       {readyToShow && questions['problems'].map((question:any, index:any) => {
