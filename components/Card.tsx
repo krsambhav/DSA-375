286136418ca99190cc12e4aa96da83efa0148b88
Kumar Sambhav <krsambhav16@gmail.com>
2022-05-28 22:53:41 +0530

    Added Data

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,8 +5,8 @@ export default function Card({ topic, title, qty }: { topic:string, title: strin
     <Link href={'/quest/'+topic.toLowerCase()}>
       <a>
       <div className="border-[0.1px] border-black w-[240px] h-[150px] flex flex-col justify-center items-center text-2xl cursor-pointer card transition-all duration-200">
-        <div className="title">{title}</div>
-        <div className="qty text-lg mt-5">{qty + " Questions"}</div>
+        <div className="title text-xl">{title}</div>
+        <div className="qty text-base mt-5">{qty + " Questions"}</div>
       </div>
       </a>
     </Link>
