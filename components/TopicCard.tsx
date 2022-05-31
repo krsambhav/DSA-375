5ad8a723dfc3d734d5d66b9d780865372e35bcb4
Kumar Sambhav <krsambhav16@gmail.com>
2022-05-31 23:38:26 +0530

    Updates

--- components/TopicCard.tsx
+++ components/TopicCard.tsx
@@ -18,7 +18,7 @@ export default function Card({ topic, title, qty, index }: { topic:string, title
   return (
     <Link href={'/quest/'+topic.toLowerCase()}>
       <a>
-      <div className="border-[0.1px] border-black w-[240px] h-[150px] flex flex-col justify-center items-center text-2xl cursor-pointer card transition-all duration-200 dark:border-white dark:hover:border-orange-200 normalcard">
+      <div className="border-[0.1px] border-black w-[240px] h-[150px] flex flex-col justify-center items-center text-2xl cursor-pointer card transition-all duration-200 hover:border-[1px] dark:border-white dark:hover:border-orange-200 topic-card hover:shadow-lg">
         <div className="title text-xl">{title}</div>
         <div className="qty text-base mt-5 font-light">{`${doneCount} / ${qty} Problems Solved`}</div>
       </div>
