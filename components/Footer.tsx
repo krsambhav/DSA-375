28378f772819bf47ea551dbc72a828b468999109
Kumar Sambhav <krsambhav16@gmail.com>
2022-06-06 19:44:45 +0530

    Updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,19 @@
 import { BsFileEarmarkSpreadsheet } from "react-icons/bs";
 import { VscGithubAlt } from "react-icons/vsc";
 import ThemeChanger from "./ThemeChanger";
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-export default function Footer ({doneQuestions, totalQuestions}:{doneQuestions?:number;totalQuestions?:number}) {
+export default function Footer({
+  doneQuestions,
+  totalQuestions,
+}: {
+  doneQuestions?: number;
+  totalQuestions?: number;
+}) {
   return (
     <footer className="mt-10 md:mt-0 w-screen flex flow-row items-center justify-center md:h-[60px] gap-10 md:fixed md:bottom-0 transition-all duration-300 bg-white dark:bg-slate-900 pb-10 md:pb-0">
-        <div className="middle-items-container flex flow-row items-center justify-center gap-10">
+      <div className="middle-items-container flex flow-row items-center justify-center gap-10">
         <a
           href="https://github.com/krsambhav/DSA-375/"
           target={"_blank"}
@@ -21,8 +29,24 @@ export default function Footer ({doneQuestions, totalQuestions}:{doneQuestions?:
         >
           <BsFileEarmarkSpreadsheet className="text-lg" />
         </a>
+      </div>
+
+      {doneQuestions && totalQuestions && (
+        <div className="done-count-container text-black dark:text-white select-none self-end hidden md:fixed right-10 bottom-4 md:flex flex-row items-center gap-3">
+          {" "}
+          <div className="progress-circle w-[20px]">
+            <CircularProgressbar
+              value={(doneQuestions / totalQuestions) * 100}
+              strokeWidth={50}
+              styles={buildStyles({
+                strokeLinecap: "butt",
+                pathColor: "red",
+              })}
+            />
+          </div>{" "}
+          {doneQuestions} / {totalQuestions} Solved
         </div>
-        {doneQuestions && <div className="done-count-container text-black dark:text-white select-none self-end hidden md:block md:fixed right-10 bottom-4">{doneQuestions} / {totalQuestions} Solved</div>}
-      </footer>
-  )
+      )}
+    </footer>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,27 @@ import { IoLogoGithub } from "react-icons/io";
 import ThemeChanger from "../components/ThemeChanger";
 import { SiGooglesheets } from "react-icons/si";
 import Footer from "../components/Footer";
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [totalSolved, setTotalSolved] = useState<number>()
+  const [totalProblems, settotalProblems] = useState<number>()
+  useEffect(() => {
+    if(localStorage.getItem('progressData') === null) return;
+    const progressData = JSON.parse(localStorage.getItem('progressData') || "{}")
+    let solvedCount = 0;
+    let totalProblems = 0;
+    progressData.map((topic:any) => {
+      topic.problems.map((problem:any) => {
+        totalProblems++;
+        problem.done && solvedCount++;
+      })
+    })
+    setTotalSolved(solvedCount);
+    settotalProblems(totalProblems);
+  }, [])
   return (
     <div className="transition-all duration-300 bg-white dark:bg-slate-900 pb-10">
       <Head>
@@ -23,10 +42,35 @@ const Home: NextPage = () => {
         <a
           href="https://www.youtube.com/c/ApnaCollegeOfficial"
           target={"_blank"}
-          rel='noreferrer'
+          rel="noreferrer"
         >
-          <div className="text-sm border-b py-[1px] border-gray-500 hover:border-black hover:px-[3px] text-gray-500 hover:text-black transition-all duration-150 dark:text-gray-400 dark:border-gray-400 dark:hover:text-white dark:hover:border-white">By Apna College</div>
+          <div className="text-sm border-b py-[1px] border-gray-500 hover:border-black hover:px-[3px] text-gray-500 hover:text-black transition-all duration-150 dark:text-gray-400 dark:border-gray-400 dark:hover:text-white dark:hover:border-white">
+            By Apna College
+          </div>
         </a>
+        {totalSolved && totalProblems ? <div className="progress-container mt-10 w-[100px]">
+          <CircularProgressbar
+            value={totalSolved/totalProblems*100}
+            text={`${Math.round(totalSolved/totalProblems*100)}% Solved`}
+            styles={buildStyles({
+              strokeLinecap: "butt",
+              textSize: "14",
+              textColor: "#AAA",
+              pathColor: "#ff6d6d",
+            })}
+          />
+        </div> : <div className="progress-container mt-10 w-[100px]">
+          <CircularProgressbar
+            value={0}
+            text={`Start Solving`}
+            styles={buildStyles({
+              strokeLinecap: "butt",
+              textSize: "12",
+              textColor: "#AAA",
+              pathColor: "black"
+            })}
+          />
+        </div>}
         <div className="cards-container mt-10 md:mt-12 flex flex-row flex-wrap justify-center gap-10">
           {questionData.map((topic, index) => {
             return (
