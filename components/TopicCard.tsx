b77d0cc188e9fa64d077428c5df2a9c26d579912
Kumar Sambhav <krsambhav16@gmail.com>
2022-12-01 20:57:34 +0530

    Updates

--- components/TopicCard.tsx
+++ components/TopicCard.tsx
@@ -20,10 +20,10 @@ export default function Card({ topic, title, qty, index }: { topic:string, title
   return (
     <Link href={'/quest/'+topic.toLowerCase()}>
       <a>
-      <div className="border-[1px] border-black w-[240px] h-[150px] flex flex-col justify-center items-center text-2xl cursor-pointer card transition-shadow hover:border-[1px] dark:border-[#ffdeb4] dark:hover:border-orange-200 topic-card hover:shadow-lg">
+      <div className={`border-[1px] border-black w-[240px] h-[150px] flex flex-col justify-center items-center text-2xl cursor-pointer card transition-shadow hover:border-[1px] dark:border-[#ffdeb4] dark:hover:border-orange-200 topic-card hover:shadow-lg ${doneCount == qty && 'bg-black text-white dark:bg-orange-200 dark:text-black'}`}>
         <div className="title text-xl">{title}</div>
         <div className="qty text-base mt-5 font-light">{`${doneCount} / ${qty} Problems Solved`}</div>
-        {doneCount > 0 ? <div className="qty text-base mt-5 font-light">{`${qty - doneCount} Remaining`}</div> : <div className="qty text-base mt-5 font-light">{`Not Started Yet`}</div>} 
+        {doneCount > 0 ? <div className="qty text-base mt-5 font-light">{`${qty == doneCount ? 'Killed It' : qty - doneCount + ' Remaining'}`}</div> : <div className="qty text-base mt-5 font-light">{`Not Started Yet`}</div>} 
       </div>
       </a>
     </Link>
